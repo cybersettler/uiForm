87e55e7accb9988cf32b6a9bc8886a19c1a36b29
Javier escobar <cybersettler@babylonone.com>
2017-01-05 14:25:04 +0100

    Now compatible with websemble 2.0

--- component/ui/Form/Controller.js
+++ component/ui/Form/Controller.js
@@ -5,54 +5,61 @@ const tv4 = require("tv4");
 /**
  * Controller of the form widget
  * @constructor
- * @param {object} view - HTML element
- * @param {object} model - Model to keep state data
+ * @param {object} view - HTML element.
+ * @param {object} scope - Context of the web component.
  */
-function FormController(view, model) {
-  this.super(view, model);
+function FormController(view, scope) {
+  this.super(view, scope);
 
-  // Fires when an instance was inserted into the document
-  this.onAttached = function(){
-    view.addEventListener("submit", function(e) {
-      console.log("form submitted", e);
-    });
-  };
+  var form = view.shadowRoot.querySelector('form');
 
-  this.onAttributeChanged = function() {
-    this.updateView();
-  };
+  view.addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log("form submitted", e);
+  });
 
-  this.updateView = function() {
-    if (!view.hasAttribute('data-schema')) { return; }
-    this.getAttributeValueFromParentScope("schema").then(init);
-  };
+  if (view.hasAttribute('data-schema')) {
+    scope.getAttributeValueFromParentScope("schema").then(init);
+  }
 
   function init(schema) {
+    var idMap = {};
+    var model = {};
+
     // Update…
-    var group = d3.select(view.shadowRoot)
+    var group = d3.select(view.shadowRoot.querySelector("form"))
     .selectAll("div.form-group")
     .data(Object.keys(schema.properties))
     .text(function(d) { return d; });
 
     // Enter…
-    var appended = group.enter().append("div.form-group");
+    var appended = group.enter().insert('div','content');
+
+    appended.classed("form-group", true);
 
     appended.append("label")
     .attr("for", function(d) {
       var id = shortid.generate();
-      model[d].id = id;
+      idMap[d] = id;
       return id;
     }).text(function(d) {
       return schema.properties[d].title;
     });
 
     appended.append("input")
+    .classed("form-control",true)
     .attr("name", function(d) {
       return d;
     }).attr("value", function(d) {
       return model[d];
     }).attr("id", function(d) {
-      return model[d].id;
+      return idMap[d];
+    });
+
+    appended.append("p")
+    .classed('help-block', true)
+    .text(function(d) {
+      return schema.properties[d].description;
     });
 
     // Exit…
